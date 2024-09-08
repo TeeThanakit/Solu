1c1e90e719f2d8112743a6c45cbab0c0a5856ff6
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-09-08 11:42:36 +0700

    update rename .c to .md file

--- changeFileName.sh
+++ changeFileName.sh
@@ -1,1 +1,13 @@
-find ./docs -type f -name '*.c' -exec sh -c 'mv "$0" "${0%.c}.md"' {} \;
+#!/bin/bash
+# Script to find all 'docs' folders in subdirectories and rename .c files to .md
+
+# Find all 'docs' directories in subdirectories, then find and rename .c files to .md
+find . -type d -name 'docs' -exec sh -c '
+for docs_dir; do
+    find "$docs_dir" -type f -name "*.c" -exec sh -c '"'"'
+    for file do
+        mv "$file" "${file%.c}.md"
+    done
+    '"'"' sh {} +
+done
+' sh {} +
