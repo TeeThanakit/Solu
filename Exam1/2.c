cfefd39bb1b65f128dd0eb087221c53f5fb7513b
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-09-26 12:31:25 +0700

    fix: wording

--- Exam1/2.c
+++ Exam1/2.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int n = 30;
+    int n;
     int count = 0;
     scanf("%d", &n);
 
-    for(int i = 2; i <= n;i++)
+    for(int i = 1; i <= n;i++)
     {
         int sum = 0;
         for(int x = 1; x < i; x++)
         {
             if(i % x == 0)
             {
-                sum = sum + x;
+                sum += x;
             }
         }
 
@@ -26,7 +26,49 @@ int main()
 
     if(count == 0)
     {
-        printf("No perfect number");
+        printf("No perfect number.");
     }
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     int count = 0;
+    
+//     scanf("%d", &n);
+
+//     for (int i = 2; i <= n; i += 2) 
+//     {
+//         int sum = 1;
+        
+//         for (int x = 2; x * x <= i; x++) 
+//         {
+//             if (i % x == 0) 
+//             {
+//                 sum += x; 
+                
+//                 if (x != i / x) 
+//                 {
+//                     sum += i / x;
+//                 }
+//             }
+            
+//             if (sum > i) {
+//                 break;
+//             }
+//         }
+
+//         if (sum == i) {
+//             printf("%d\n", i);
+//             count++;
+//         }
+//     }
+
+//     if (count == 0) {
+//         printf("No perfect number.\n");
+//     }
+
+//     return 0;
+// }
--- Exam1/tempCodeRunnerFile.c
+++ Exam1/tempCodeRunnerFile.c
@@ -1,1 +1,32 @@
-+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    int count = 0;
+    scanf("%d", &n);
+
+    for(int i = 2; i <= n;i++)
+    {
+        int sum = 0;
+        for(int x = 1; x < i; x++)
+        {
+            if(i % x == 0)
+            {
+                sum = sum + x;
+            }
+        }
+
+        if(sum == i)
+        {
+            printf("%d\n",i);
+            count++;
+        }
+    }
+
+    if(count == 0)
+    {
+        printf("No perfect number.");
+    }
+    return 0;
+}
